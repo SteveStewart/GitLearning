2421751084682435fc1fe8423e7a74f9eb99eaff
Steven Stewart <steven@woohooltd.uk>
2018-03-15 17:27:20 +0000

    Add project files.

---
+++ GitLearning/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitLearning.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
